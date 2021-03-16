784d7789bb57dfd88c7c7aa2877c26b8b07d74e9
Dharun Sivakumar <dharunvs@users.noreply.github.com>
2021-03-16 22:41:53 +0530

    Updates

--- public/client/src/App.js
+++ public/client/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
+import Register from './pages/Register/Register'
+import Login from './pages/Login/Login'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
+    <>
+    <Navbar />
     <Router>
-      <Route path='/' exact={() => <Home />} />
+      <Route path='/' exact render={() => <Home />} />
+      <Route path='/register' exact render={() => <Register />} />
+      <Route path='/login' exact render={() => <Login />} />
     </Router>
+    </>
   );
 }
 
---
+++ public/client/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            <a href="/">Home</a>
+            <a href="/register">Register</a>
+            <a href="/login">Log in</a>
+        </div>
+    )
+}
+
+export default Navbar
