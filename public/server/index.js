ad5bc41b9dc7f51e1b8a5f8940f7410d90b04133
Dharun Sivakumar <dharunvs@users.noreply.github.com>
2021-03-17 18:01:51 +0530

    Update index.js

--- public/server/index.js
+++ public/server/index.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const app = express();
 
-const mysql      = require('mysql');
+const mysql = require('mysql');
 const db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'Dharun@123',
-    database : 'social_media_new',
-    insecureAuth : true
+    host: 'localhost',
+    user: 'root',
+    password: 'Dharun@123',
+    database: 'social_media_new',
 });
 
 app.get('/register', (req, res) => {
-    db.query("INSERT INTO users (username, password) VALUES ('Dharun', 'hello@123')",
-    (err, results) => {
-        console.log(err);
-        res.send(results);
-    })
+    db.query("INSERT INTO users (username, password) VALUES ('Maria', 'legend@2002')",
+    (errors, results) => {
+        console.log(errors);
+        res.send(results)
+    }
+    )
 })
+
 app.listen(3001, (req, res) => {
     console.log("Server is running...");
 })
