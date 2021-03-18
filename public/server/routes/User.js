6b35bbf7ee65abf7e378e5f03105af4fec8e76d6
Dharun Sivakumar <dharunvs@users.noreply.github.com>
2021-03-18 09:54:29 +0530

    Update User.js

--- public/server/routes/User.js
+++ public/server/routes/User.js
@@ -8,10 +8,12 @@ router.post('/register', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    db.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, password],
-    (errors, results) => {
-        console.log(errors);
-        res.send(results)
+    db.query(
+        "INSERT INTO users (username, password) VALUES (?, ?)",
+        [username, password],
+        (errors, results) => {
+            console.log(errors);
+            res.send(results)
     }
     )
 })
@@ -21,7 +23,25 @@ router.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    res.send(username)
+    db.query(
+        "SELECT * FROM users WHERE username = ?",
+        username,
+        (errors, results) => {
+            if (errors){
+                console.log(errors)
+            }
+            if (results){
+                if (password == results[0].password){
+                    res.send("You are logged in!")
+                } else {
+                    res.send("Wrong password!")
+                }
+            } else {
+                res.send("Invalid username!")
+            }
+            res.send(results)
+    }
+    )
 })
 
 module.exports = router;
